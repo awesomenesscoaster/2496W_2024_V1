6b24f9c8926370feb66a493675bade81ee5fdd4b
Rohan H <rohan.hablani@gmail.com>
2024-09-17 18:35:58 -0700

    temps

--- 2496W_V1/src/main.cpp
+++ 2496W_V1/src/main.cpp
@@ -6,6 +6,7 @@
 #include "pros/motors.h"
 #include "robot.h"
 #include "piston.h"
+using namespace std;
 
 void on_center_button() {}
 
@@ -23,18 +24,29 @@ void competition_initialize() {}
 void autonomous() {}
 
 void opcontrol() {
-  long long time = 0;
+  long long time = 0; 
+  //ik theres a better way to do this 😿
+  double chassis_temp = (lf.get_temperature() + lm.get_temperature() + lb.get_temperature() + rf.get_temperature() + rm.get_temperature() + rb.get_temperature()) / 6;
+
   set_brake_coast();
   
   controller.clear();
   rotation.reset_position();
-
+  
   while (true) 
   {
+    controller.print(1, 0, "Temps: %f, %f", round(intake.get_temperature()), round(first_stage.get_temperature()));
+    if (time%2 == 0){ 
+      controller.print(2, 0, "Chassis Temp: %f", round(chassis_temp));
+    }
     
     driver();
     rotation_val(time);    
     pros::delay(2);
     time += 2;
+    // if(intake.get_temperature() >= 50) {
+    //   controller.rumble("-");
+    // }
+
   }
 }
